56dd55c257351fcaf6deecf9ade79008a5fa18fd
Wil Aquino <waquino@berkeley.edu>
2021-04-12 22:49:25 -0700

    Prepping for DraftLog

--- src/main/java/bot/Engine/DraftLog.java
+++ src/main/java/bot/Engine/DraftLog.java
@@ -1,12 +1,21 @@
 package bot.Engine;
 
-import com.google.api.services.sheets.v4.model.ValueRange;
+import bot.Discord;
+import bot.Events;
+
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.Role;
 
+import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.api.services.sheets.v4.Sheets.Spreadsheets.Values;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.TreeMap;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.List;
 
 /**
  * @author  Wil Aquino
@@ -32,26 +41,5 @@ public class DraftLog implements Command {
         } else {
             inChannel.sendMessage("The loss report was made.").queue();
         }
-
-
-        Member graduate = users.get(0);
-
-//        try {
-//            sheetsService = getSheetsService();
-//            String range = "grads!A2:B3";
-//
-//            ValueRange response = sheetsService.spreadsheets().values().get(
-//                    SPREADSHEET_ID, range).execute();
-//
-//            List<List<Object>> values = response.getValues();
-//
-//            if (values == null || values.isEmpty()) {
-//                System.out.println("No spreadhsheet data was found.");
-//            } else {
-//                for ()
-//            }
-//        } catch (IOException | GeneralSecurityException e) {
-//            outputError(inChannel, "The spreadsheet could not be accessed.");
-//        }
     }
 }
