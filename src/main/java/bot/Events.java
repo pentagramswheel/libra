7ee51c005355f3a35561896c91de767d920362ab
Wil Aquino <waquino@berkeley.edu>
2021-06-26 04:44:28 -0700

    Imported a single module from Java's AWT library, as opposed to the full library

--- src/main/java/bot/Events.java
+++ src/main/java/bot/Events.java
@@ -13,7 +13,7 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.awt.*;
+import java.awt.Color;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
