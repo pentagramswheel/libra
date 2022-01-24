590ce167afe94fefcc30287cdef68757d4cf9452
Wil Aquino <waquino@berkeley.edu>
2022-01-24 08:35:11 -0800

    Unnecessary libraries

--- src/main/java/bot/Events.java
+++ src/main/java/bot/Events.java
@@ -23,8 +23,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.Color;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
 
